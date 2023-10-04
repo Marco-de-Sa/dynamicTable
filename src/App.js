6b4a158ed623600984354a957b9ee6e74840c69e
Marco-de-Sa <marcojb.desa@gmail.com>
2023-10-04 12:07:29 +0200

    second commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import DynamicTable from "./dynamic-table/DynamicTable";
+import React from 'react';
+
+function App() {
+  return (
+    <DynamicTable />
+  );
+}
+
+export default App;
